990426ae3fc4a5d7035ce4be4878053209ec87a7
aantle11 <alicia.antle@keyin.com>
2025-07-19 22:36:37 -0230

    drug.java complete

---
+++ Drug.java
@@ -0,0 +1,22 @@
+public class Drug {
+    private int drugId;
+    private String drugName;
+    private double drugCost;
+    private String dosage;
+
+    public Drug(int drugId, String drugName, double drugCost, String dosage) {
+        this.drugId = drugId;
+        this.drugName = drugName;
+        this.drugCost = drugCost;
+        this.dosage = dosage;
+    }
+
+    public String toString() {
+        return drugId + "," + drugName + "," + drugCost + "," + dosage;
+    }
+
+    public static Drug fromString(String line) {
+        String[] parts = line.split(",");
+        return new Drug(Integer.parseInt(parts[0]), parts[1], Double.parseDouble(parts[2]), parts[3]);
+    }
+}
