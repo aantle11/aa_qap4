d92cde25348e22d5110dcff95eb80ae530c5202d
aantle11 <alicia.antle@keyin.com>
2025-07-19 23:49:32 -0230

    File I/O ( DrugManager) Complete

--- DrugManager.java
+++ DrugManager.java
@@ -1,3 +1,27 @@
+import java.io.*;
+import java.util.*;
+
 public class DrugManager {
-    
+    private static final String FILE_NAME = "drugs.txt";
+
+    public static void saveDrugsToFile(List<Drug> drugs) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
+            for (Drug drug : drugs) {
+                writer.write(drug.toString());
+                writer.newLine();
+            }
+        } 
+    }
+
+    public static List<Drug> readDrugsFromFile() throws IOException {
+        List<Drug> drugs = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                drugs.add(Drug.fromString(line));
+            }
+        }
+
+        return drugs;
+    }
 }
