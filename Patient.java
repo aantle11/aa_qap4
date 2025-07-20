dc800c956169c5f96a9fa01ee98b22519f3a4349
aantle11 <alicia.antle@keyin.com>
2025-07-19 23:01:56 -0230

    Patient.java complete

--- Patient.java
+++ Patient.java
@@ -1,3 +1,17 @@
 public class Patient {
-    
+    private int patientId;
+    private String firstName;
+    private String lastName;
+    private String dob;
+
+    public Patient(int id, String fName, String lName, String dob) {
+        this.patientId = id;
+        this.firstName = fName;
+        this.lastName = lName;
+        this.dob = dob;
+    }
+
+    public String toString() {
+        return patientId + "-" + firstName + "" + lastName + "(DOB: " + dob + ")";
+    }
 }
